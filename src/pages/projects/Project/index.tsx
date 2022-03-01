32bd489abb900a0aa6f2059c80b934cc9ee34bea
zeweilin1993 <347369684@qq.com>
2022-03-01 13:53:52 +0800

    projects样式修改

--- src/pages/projects/Project/index.tsx
+++ src/pages/projects/Project/index.tsx
@@ -7,17 +7,13 @@ function Project({ project }: IProjectProps) {
       <div className='left'>
         <a href={project.url} rel='noreferrer' target='_blank'>
           <img src={project.logo} alt={project.name} />
-          <span className='bd_n1'></span>
-          <span className='bd_n2'></span>
-          <span className='bd_n3'></span>
-          <span className='bd_n4'></span>
         </a>
 
       </div>
       <div className='right'>
-        <p>Name: {project.name}</p>
-        <p>Skills: {project.skills}</p>
-        <p>Desc: {project.desc}</p>
+        <div className='prj-info-item'>Name: <a href={project.url} rel='noreferrer' target='_blank'>{project.name}</a></div>
+        <div className='prj-info-item'>Skills: <p>{project.skills}</p></div>
+        <div className='prj-info-item'>Desc: <p>{project.desc}</p></div>
       </div>
     </div>
   );
